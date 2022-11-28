00605fe8ad9fc9abd5769fd6d39a995bc9f3e6e1
hami-r <mynameissaitama54@gmail.com>
2022-11-28 20:09:07 +0530

    read values from add posts page

--- src/app/add-posts/add-posts.component.ts
+++ src/app/add-posts/add-posts.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./add-posts.component.css']
 })
 export class AddPostsComponent {
+  
+  userId = ""
+  id = ""
+  title = ""
+  body = ""
 
+  readValues = () => {
+    let data:any = {
+      "userId": this.userId,
+      "id": this.id,
+      "title": this.title,
+      "body": this.body
+    }
+
+    console.log(data);
+    
+  }
 }
